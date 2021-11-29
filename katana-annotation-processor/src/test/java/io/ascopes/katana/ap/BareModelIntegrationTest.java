622aace576282b520be85b494e5e4a0045759f4e
Ashley Scopes <ashleyscopes@outlook.com>
2021-11-29 15:07:05 +0000

    Fix test not running

--- katana-annotation-processor/src/test/java/io/ascopes/katana/ap/BareModelIntegrationTest.java
+++ katana-annotation-processor/src/test/java/io/ascopes/katana/ap/BareModelIntegrationTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 import static com.google.testing.compile.CompilationSubject.assertThat;
 import static com.google.testing.compile.JavaFileObjects.forSourceLines;
 
-class BareModelTest {
+class BareModelIntegrationTest {
 
   KatanaProcessor processor;
 
