53ad05998034020dd7ec73f89dc28d2894b1c2ce
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2021-12-14 08:25:55 +0000

    Update build-in-docker.sh

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -4,6 +4,9 @@
 ### Helper script to enable me to debug builds on certain targets without having to
 ### physically install them on my machine.
 ###
+### This is also used by the CI to enable the use of pre-release JDK images, and to
+### enable being able to replicate builds from CI on your local machine.
+###
 
 if [ $# -lt 1 ]; then
   echo "USAGE: $0 <java version | image reference> [flags to pass to Maven]"
@@ -14,7 +17,7 @@ set -e
 set -x
 
 if echo "${1}" | grep -qE '^[0-9]+$'; then
-  image="openjdk:${1}-alpine"
+  image="openjdk:${1}"
 else
   image="${1}"
 fi
@@ -25,9 +28,12 @@ group_id="$(id -g "${USER}")"
 shift 1
 
 docker run \
-  --rm \
-  -u "${user_id}:${group_id}" \
-  -v "${this_dir}:${this_dir}" \
-  -w "${this_dir}" \
-  "${image}" \
-  "${this_dir}/mvnw" $@
+    --rm \
+    -u "${user_id}:${group_id}" \
+    -v "${this_dir}:${this_dir}" \
+    -w "${this_dir}" \
+    "${image}" \
+    "${this_dir}/mvnw" $@ \
+    2>&1 \
+  | while read line; do echo -e "\e[1;36m[DOCKER]\e[0m ${line}"; done
+  
