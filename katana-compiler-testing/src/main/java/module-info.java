185a2b14ddc343af421bd4228b09308a7b68de33
Ashley Scopes <ashleyscopes@outlook.com>
2022-01-05 10:29:08 +0000

    Add note to handle modules correctly for jimfs and fuzzywuzzy
    Depends on:
      - https://github.com/xdrop/fuzzywuzzy/pull/95
      - https://github.com/google/jimfs/pull/180

--- katana-compiler-testing/src/main/java/module-info.java
+++ katana-compiler-testing/src/main/java/module-info.java
@@ -1,11 +1,19 @@
 module katana.compilertesting {
-  requires fuzzywuzzy;
   requires java.base;
   requires java.compiler;
-  requires jimfs;
   requires kotlin.stdlib.jdk7;
   requires kotlin.stdlib.jdk8;
   requires org.opentest4j;
 
   exports io.ascopes.katana.compilertesting.java;
+
+  /////////////////////////////////////////////////////////////////////
+  /// Modules that need updating when a module release is available ///
+  /////////////////////////////////////////////////////////////////////
+
+  // https://github.com/xdrop/fuzzywuzzy/pull/95
+  requires fuzzywuzzy;
+
+  // https://github.com/google/jimfs/pull/180
+  requires jimfs;
 }
